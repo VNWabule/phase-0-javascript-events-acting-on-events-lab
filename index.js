8b22ea55d4c46aaaf700d85ba3fe6aa6b6800735
VNWabule <vicwabule@gmail.com>
2025-03-06 14:47:54 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // Assuming the game area width is 400px and dodger width is 40px
+    dodger.style.left = `${left + 1}px`;
+  }
+}
